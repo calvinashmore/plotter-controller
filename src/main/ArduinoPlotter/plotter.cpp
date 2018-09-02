2975b50b26d7f70c8414df4038a927802a297ad2
calvinashmore <ashmore@gmail.com>
2018-09-02 13:58:38 -0700

    untested limit switches

--- src/main/ArduinoPlotter/plotter.cpp
+++ src/main/ArduinoPlotter/plotter.cpp
@@ -31,6 +31,10 @@ Servo servo2; // pressure
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 #define ENABLE_PIN 38
+#define LIMIT_X_MIN_PIN 42
+#define LIMIT_X_MAX_PIN 46
+#define LIMIT_Y_MIN_PIN 40
+#define LIMIT_Y_MAX_PIN 44
 #define DELTA 5
 
 #define MIN_STEPPER_SPEED 100
@@ -41,6 +45,14 @@ Data currentData;
 Data nextData;
 bool hasData = false;
 bool isStarted = false;
+bool limitXMinReached = false;
+bool limitXMaxReached = false;
+bool limitYMinReached = false;
+bool limitYMaxReached = false;
+longlong limitXMinValue;
+longlong limitXMaxValue;
+longlong limitYMinValue;
+longlong limitYMaxValue;
 
 // "private" methods
 void enableSteppers(boolean);
@@ -53,6 +65,7 @@ DECL_RPC_CALLBACK7(handleData, longlong, float, float, float, float, float, floa
 DECL_RPC_CALLBACK2(moveXY, float, float);
 DECL_RPC_CALLBACK2(moveAxis, int, float);
 IMPL_RPC_CALL1(reportProgress, longlong);
+IMPL_RPC_CALL3(reportLimit, int, int, longlong); // axis, max vs min, value
 
 void setup() {
   
@@ -73,10 +86,16 @@ void setup() {
   pinMode(ENABLE_PIN, OUTPUT);
   servo1.attach(34);
   servo2.attach(36);
+  
+  pinMode(LIMIT_X_MIN_PIN, INPUT);
+  pinMode(LIMIT_X_MAX_PIN, INPUT);
+  pinMode(LIMIT_Y_MIN_PIN, INPUT);
+  pinMode(LIMIT_Y_MAX_PIN, INPUT);
 }
 
 void loop() {
   if (isStarted) {
+    checkLimits();
     enableSteppers(true);
     if (hasData) {
       doStuff();
@@ -106,6 +125,20 @@ void doStuff() {
   stepperCoordinates[0] = (long)(100*currentData.position_x);
   stepperCoordinates[1] = (long)(100*currentData.position_y);
   stepperCoordinates[2] = (long)(100*currentData.yaw);
+
+  // Apply limits
+  if (limitXMinReached) {
+    stepperCoordinates[0] = max(stepperCoordinates[0], limitXMinValue);
+  }
+  if (limitXMaxReached) {
+    stepperCoordinates[0] = min(stepperCoordinates[0], limitXMaxValue);
+  }
+  if (limitYMinReached) {
+    stepperCoordinates[1] = max(stepperCoordinates[1], limitYMinValue);
+  }
+  if (limitYMaxReached) {
+    stepperCoordinates[1] = min(stepperCoordinates[1], limitYMaxValue);
+  }
   
   // speed in steps/second
   // > 1000 are unreliable.
@@ -205,6 +238,30 @@ void moveAxis(int axis, float value) {
   }
   nextData = currentData;
   hasData = true;
+}
+
+void checkLimits() {
+  if (digitalRead(LIMIT_X_MIN_PIN) && !limitXMinReached) {
+    limitXMinReached = true;
+    limitXMinValue = stepper1.currentPosition();
+    reportLimit(0, -1, limitXMinValue);
+  }
+  if (digitalRead(LIMIT_X_MAX_PIN) && !limitXMaxReached) {
+    limitXMaxReached = true;
+    limitXMaxValue = stepper1.currentPosition();
+    reportLimit(0, 1, limitXMaxValue);
+  }
+  
+  if (digitalRead(LIMIT_Y_MIN_PIN) && !limitYMinReached) {
+    limitYMinReached = true;
+    limitYMinValue = stepper2.currentPosition();
+    reportLimit(1, -1, limitYMinValue);
+  }
+  if (digitalRead(LIMIT_Y_MAX_PIN) && !limitYMaxReached) {
+    limitYMaxReached = true;
+    limitYMaxValue = stepper2.currentPosition();
+    reportLimit(1, 1, limitYMaxValue);
+  }
 }
 
 } // namespace plotter
